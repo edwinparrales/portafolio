403bc656bf7147f7b7e9d5f55f068b0296265f27
edwinparrales <edwinparrales82@gmail.com>
2024-04-08 22:48:24 -0500

    Iniciando el portafolio web

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {CarruselComponent} from "../../components-ui/carrusel/carrusel.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CarruselComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
